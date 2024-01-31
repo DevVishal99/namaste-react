d308c0dd8938c1c66ed01469c746ce4a20ef7292
Vishal Balekundri <vishalbalekundri88@gmail.com>
2024-01-31 20:08:54 +0530

    Episode 5 Code

--- scr/components/Body.js
+++ scr/components/Body.js
@@ -1,13 +1,32 @@
 import resList from "../utlis/mockData";
 import ResCard from "./ResCard";
+import { useState } from "react";
+
+
+
+
 
 const Body = () => {
+    const [listOfRestorents, setListOfRestorents] = useState(resList)
+
     return (
-        <><div className="search">Search</div>
+        <><div className="filter-btn">
+            <button onClick={() => {
+                    const filteredList = listOfRestorents.filter(
+                        (res) => res.data.avgRating >= 4
+                    );
+
+                    setListOfRestorents(filteredList)
+                    
+
+                }}
+                >
+                Top Rated Restorunts</button>
+        </div>
 
         <div className="res-container">
             {
-                resList.map((restaurant)=>
+                listOfRestorents.map((restaurant)=>
                     (<ResCard key={restaurant.data.id} resData={restaurant} />)
                 )
             }
